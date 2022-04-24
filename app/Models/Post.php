90f2a9a5f71c96138af95040b7741022c40f2e8b
JuniorGuita <junior.siqueira.ti@gmail.com>
2022-04-24 14:23:00 -0300

    Atualizando a documentação

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,16 +5,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-
-/**
- * @OA\Schema(
- *     title="Post",
- *     description="Descrição dos elementos do model Post",
- *     @OA\Xml(
- *         name="Post"
- *     )
- * )
- */
 class Post extends Model
 {
     use HasFactory;
